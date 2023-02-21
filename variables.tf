c620d67fbaeb66dac8c8543c4475c05ab75bc87c
Devin Lauderdale <devin.lauderdale@synapsestudios.com>
2023-02-21 10:30:28 -0600

    remove region variable

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  type        = string
-  default     = "us-west-2"
-  description = "Define the region you'd wish the cloudtrail resources to be created in, example: us-west-2"
-}
-
 variable "namespace" {
   type        = string
   description = "Define the namespace here, example: foobarinc-dev (projectname-environment)"
