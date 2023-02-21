8239111445dadeb0c9aae2503d3e767ff82f8bbe
Devin Lauderdale <devin.lauderdale@synapsestudios.com>
2023-02-21 10:26:55 -0600

    remove provider block from module

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
 #Create CloudTrail trail
